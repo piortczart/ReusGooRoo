ce17f1a85ca305d2f984a7b57fb924b8c49b861f
PiortCzart <no@email.nono>
2017-02-16 01:25:58 +0100

    Moved classes to a new file, started coding logic...

---
+++ app/components/game_stuff/game_classes.js
@@ -0,0 +1,59 @@
+angular.module('myApp').factory('Giant', function (ValueMapper, GiantSlot) {
+    function Giant() {
+        this.slots = [new GiantSlot(), new GiantSlot(), new GiantSlot(), new GiantSlot()];
+    };
+
+    Giant.fromJson = function (jsonObject) {
+        return ValueMapper(new Giant(), jsonObject);
+    }
+
+    function has_ambassador(first_letter) {
+        if (first_letter == 'a')
+            return true;
+        for (var i = 0; i < 4; i++) {
+            if (this.slots[i].ambassador[i] == first_letter)
+                return true;
+        }
+        return false;
+    }
+
+    Giant.prototype.get_active_abilities = function() {
+        return this.abilities.filter(function (ability) {
+            return ability.requires_ambassadors[0] == ""
+                || ability.requires_ambassadors[0].split('')
+        });
+    }
+
+    // Return the constructor.
+    // So in fact what is then injected into different services is a constructor function.
+    return Giant;
+});
+
+angular.module('myApp').factory('GiantSlot', function () {
+    function GiantSlot() {
+    };
+    return GiantSlot;
+});
+
+angular.module('myApp').factory('Ambassador', function (ValueMapper) {
+    function Ambassador() {
+    }
+
+    Ambassador.fromJson = function (jsonObject) {
+        return ValueMapper(new Ambassador(), jsonObject);
+    }
+
+    // Return the constructor.
+    return Ambassador;
+});
+
+angular.module('myApp').factory('Resource', function (ValueMapper) {
+    function Resource() {
+    }
+
+    Resource.fromJson = function (jsonObject) {
+        return ValueMapper(new Resource(), jsonObject);
+    }
+
+    return Resource;
+});
--- app/components/game_stuff/game_service.js
+++ app/components/game_stuff/game_service.js
@@ -1,29 +1,3 @@
-var Biomes = (function () {
-    function Biomes(biomesArray) {
-        this._all = biomesArray;
-    };
-
-    // Filtering function.
-    function hasGiant(biome) {
-        return biome.hasGiant;
-    }
-
-    // Filtering function.
-    function hasAmbassador(biome) {
-        return biome.hasAmbassador;
-    }
-
-    Biomes.prototype.withGiants = function () {
-        return this._all.filter(hasGiant);
-    };
-
-    Biomes.prototype.withAmbassadors = function () {
-        return this._all.filter(hasAmbassador);
-    };
-
-    return Biomes;
-})();
-
 angular.module('myApp').value('ValueMapper', function (target, source) {
     for (var prop in source) {
         if (source.hasOwnProperty(prop)) {
@@ -31,49 +5,6 @@ angular.module('myApp').value('ValueMapper', function (target, source) {
         }
     }
     return target;
-});
-
-angular.module('myApp').factory('Giant', function (ValueMapper, GiantSlot) {
-    function Giant() {
-        this.slots = [new GiantSlot(), new GiantSlot(), new GiantSlot(), new GiantSlot()];
-    };
-
-    Giant.fromJson = function (jsonObject) {
-        return ValueMapper(new Giant(), jsonObject);
-    }
-
-    // Return the constructor.
-    // So in fact what is then injected into different services is a constructor function.
-    return Giant;
-});
-
-angular.module('myApp').factory('GiantSlot', function () {
-    function GiantSlot() {
-    };
-    return GiantSlot;
-});
-
-angular.module('myApp').factory('Ambassador', function (ValueMapper) {
-    function Ambassador() {
-    }
-
-    Ambassador.fromJson = function (jsonObject) {
-        return ValueMapper(new Ambassador(), jsonObject);
-    }
-
-    // Return the constructor.
-    return Ambassador;
-});
-
-angular.module('myApp').factory('Resource', function (ValueMapper) {
-    function Resource() {
-    }
-
-    Resource.fromJson = function (jsonObject) {
-        return ValueMapper(new Resource(), jsonObject);
-    }
-
-    return Resource;
 });
 
 angular.module('myApp').factory('GameObjectsService', function ($http, Giant, Ambassador, Resource) {
